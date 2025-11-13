6b8371080903f20486f4d30e12b44fd478d43a1e
armandolezama <armando.lezama.psic@gmail.com>
2025-11-12 19:38:17 -0600

    chore(*): modified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 // vite.config.ts
-import path from "path"
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: { alias: { "@":  path.resolve(__dirname, "./src") } }, // para tus imports "@/..."
+  resolve: { alias: { "@":  fileURLToPath(new URL("./src", import.meta.url)) } }, // para tus imports "@/..."
   base: "/portfolio-web/",
 });
